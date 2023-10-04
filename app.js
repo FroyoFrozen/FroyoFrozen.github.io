46cafcbb71e737ce2d4f01e535ab68d334126ff8
FroyoFrozen <will0145@hz.nl>
2023-10-04 19:27:12 +0200

    Added a magnificent wheel

---
+++ app.js
@@ -0,0 +1,18 @@
+let progressBar = document.querySelector(".circular-progress");
+let valueContainer = document.querySelector(".value-container");
+
+let progressValue = 0;
+let progressEndValue = 60;
+let speed = 20;
+
+let progress = setInterval(() => {
+    progressValue = progressValue + 0.25;
+    valueContainer.textContent = `${progressValue}`;
+    progressBar.style.background = `conic-gradient(
+        #4d5bf9 ${progressValue * 6}deg,
+        #cadcff ${progressValue * 6}deg
+    )`
+    if(progressValue == progressEndValue) {
+        clearInterval(progress)
+    }
+}, speed);
